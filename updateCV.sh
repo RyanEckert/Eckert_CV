98a22a5458b205c20f5b821c7e17424f25d4064f
RyanEckert <ryan.j.eckert@gmail.com>
2021-01-22 16:36:46 -0500

    update cv

--- updateCV.sh
+++ updateCV.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+################################################################################
+## This shell script will render .html and .pdf versions of the CV using the  ##
+## the 'render_cv.R' script then commit updates and push the origin to GitHub ##                                                                   ##
+## Place it in the directory with 'render_cv.R' and your CV .Rmd files        ##
+## Make this executable if not already, "chmod +x updateCV.sh"                ##
+## Execute with "./updateCV.sh" while in the cv working directory             ##
+################################################################################
 
 Rscript render_cv.R
 git add *
