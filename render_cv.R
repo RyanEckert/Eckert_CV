ee46f9098691657b5cc2166338d65a642d45286a
RyanEckert <ryan.j.eckert@gmail.com>
2024-09-09 12:51:18 -0400

    update cv

--- render_cv.R
+++ render_cv.R
@@ -10,6 +10,8 @@
 if (!require("pacman")) install.packages("pacman",  repos='http://cran.us.r-project.org')
 pacman::p_load("rmarkdown", "pagedown")
 
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 rmarkdown::render("Eckert_CV.Rmd",
                   params = list(pdf_mode = FALSE),
                   output_file = "index.html")
